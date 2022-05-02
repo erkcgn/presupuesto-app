11559e37e93229a7a51da20fe8603913115625f5
Erika <erikac.ec80@gmail.com>
2022-05-02 16:42:52 -0300

    cabecero padre-hijo

--- src/app/cabecero/cabecero.component.ts
+++ src/app/cabecero/cabecero.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cabecero',
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CabeceroComponent implements OnInit {
 
+  @Input() presupuestoTotal:number;
+  @Input() ingresoTotal:number;
+  @Input() egresoTotal:number;
+  @Input() porcentajeTotal:number;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
 }
+
+
+function input() {
+  throw new Error('Function not implemented.');
+}
+
